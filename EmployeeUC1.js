431d83bea7ae27d9e9b1774049927e51becd269d
Himanshu <yadavhimanshu63@gmail.com>
2021-01-03 23:30:10 +0530

    Check Attendance

---
+++ EmployeeUC1.js
@@ -0,0 +1,10 @@
+//Random-check Employee Atteandance
+{
+    const IS_ABSENT=0;
+    let empCheck=Math.floor(Math.random()*10)%2;
+    if(empCheck==IS_ABSENT){
+        console.log("UC1_Employee is absent");
+    }else{
+        console.log("UC1_Employee is present")
+    }
+}
